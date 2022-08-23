235cee23c70849c98a5259dcfe735d2927898559
danbortolato <daniel.bortolato@gmail.com>
2022-08-23 05:56:38 -0300

    first commit

---
+++ src/main/java/com/danbortolato/godev/GodevApplication.java
@@ -0,0 +1,13 @@
+package com.danbortolato.godev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodevApplication.class, args);
+	}
+
+}
